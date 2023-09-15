e4cc560f37c384bfccbea4e6ad1ad5a6ace268e9
Landon Thull <landonthull.tx@gmail.com>
2023-09-14 21:57:23 -0500

    style: rebuild login page

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -9,12 +9,15 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Button } from "./ui/button";
+import {Separator} from "@/components/ui/separator.tsx";
 
 const LoginCard: React.FC = () => {
   return (
-    <Card className="w-full max-w-sm">
+    <Card className="w-full max-w-md h-full">
       <CardHeader>
-        <CardTitle>Login</CardTitle>
+        <h1 className="text-4xl text-center mb-4 font-bold tracking-wider">QUOTE MASTER</h1>
+        <Separator />
+        <CardTitle className="pt-4">Login</CardTitle>
         <CardDescription>
           Enter your email and password to login.
         </CardDescription>
@@ -22,15 +25,18 @@ const LoginCard: React.FC = () => {
       <CardContent className="text-muted-foreground grid gap-4">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="johndoe@email.com" />
+          <Input className="text-primary" id="email" type="email" placeholder="johndoe@email.com" />
+          <p></p>
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
+          <Input className="text-primary" id="password" type="password" />
+          <p></p>
         </div>
       </CardContent>
-      <CardFooter>
-        <Button className="w-full">Login</Button>
+      <Separator />
+      <CardFooter className="mt-4">
+        <Button className="w-full">LOGIN</Button>
       </CardFooter>
     </Card>
   );
