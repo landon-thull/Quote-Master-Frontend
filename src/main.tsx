288d734658fe6c0d41c7d015376cc167733eab81
Landon Thull <landonthull.tx@gmail.com>
2023-09-15 17:32:37 -0500

    feat: configure react-redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import {Provider} from "react-redux";
+
+import {store} from "./stores/store.ts"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="system">
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="system">
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
