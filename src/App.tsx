99e06c178232749a92f34e243d4cddc7201c8fc4
Landon Thull <landonthull.tx@gmail.com>
2023-09-14 21:56:45 -0500

    feat: remove App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import "./App.css";
 import LoginCard from "./components/LoginCard";
 
 function App() {
   return (
-    <div className="bg-background text-primary min-h-full p-8 flex justify-center items-center flex-col">
-      <h1 className="text-6xl font-bold mb-6">Quote Master</h1>
-      <p className="text-xl text-muted-foreground">Coming Soon</p>
-      <LoginCard />
-    </div>
+      <div className="bg-background text-primary min-h-full p-8 flex justify-center items-center flex-row">
+        <LoginCard/>
+      </div>
   );
 }
 
