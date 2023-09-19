39a13a6e6f4e31ce3f3886dbfe425cc7c2b6650b
Landon Thull <landonthull.tx@gmail.com>
2023-09-19 18:02:56 -0500

    feat: check jwt expiration in protected route
    check if jwt is expired before allowing user to view protected pages. Loading page temporarily displayed to prevent screen flash.

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,11 @@
 import {useAppSelector} from "@/hooks/reduxHooks.ts";
 import {Navigate} from "react-router-dom";
-import {ReactElement} from "react";
+import React, {ReactElement, useEffect, useState} from "react";
+import {useAppDispatch} from "@/hooks/reduxHooks.ts";
+import {setIsAuthenticated} from "@/stores/authSlice.ts";
+import authUtil from "@/utils/authUtil.ts";
+import storage from "@/utils/storage.ts";
+import Loading from "@/components/Loading.tsx";
 
 interface IProps {
   children: ReactElement | ReactElement[];
@@ -8,7 +13,23 @@ interface IProps {
 }
 
 const ProtectedRoute: React.FC<IProps> = ({children, requireAuth = false}) => {
+  const dispatch = useAppDispatch();
   const authState = useAppSelector((state) => state.authSlice);
+  const [authStateLoading, setAuthStateLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (authUtil.isTokenExpired()) {
+      dispatch(setIsAuthenticated(false));
+      storage.clearToken();
+    } else {
+      dispatch(setIsAuthenticated(true));
+    }
+    setAuthStateLoading(false);
+  }, [dispatch]);
+
+  if (authStateLoading) {
+    return <Loading/>;
+  }
 
   if (requireAuth && !authState.isAuthenticated) {
     return <Navigate replace to="/login"/>
