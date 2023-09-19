6796cfc579686eb1fa2d21464ece34f147e66ff7
Landon Thull <landonthull.tx@gmail.com>
2023-09-19 16:56:40 -0500

    feat: use requireAuth

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PageWrapper from "@/components/PageWrapper.tsx";
 
 function App() {
   return (
-      <PageWrapper>
+      <PageWrapper requireAuth>
         <h1 className="p-8 text-primary text-3xl">Auth Successful</h1>
       </PageWrapper>
   );
