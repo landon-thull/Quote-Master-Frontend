14f54eaf2999a9cb02976cdab5a885efe1e8aa57
Landon Thull <landonthull.tx@gmail.com>
2023-09-19 18:02:51 -0500

    fix: fix any type in auth util

--- src/utils/authUtil.ts
+++ src/utils/authUtil.ts
@@ -1,18 +1,17 @@
-import jwtDecode from "jwt-decode";
+import jwtDecode, {JwtPayload} from "jwt-decode";
 import storage from "@/utils/storage.ts";
 
 const authUtil = {
   isTokenExpired: (): boolean => {
-    let token = storage.getToken();
+    const token = storage.getToken();
     if (token == undefined) {
       return true;
     }
 
-    let decodedToken: any = jwtDecode(token.slice(6));
-    let currentDate = new Date();
-    console.log(decodedToken);
+    const decodedToken: JwtPayload = jwtDecode(token.slice(6));
+    const currentDate = new Date();
 
-    return !(decodedToken && decodedToken.exp * 1000 > currentDate.getTime());
+    return !(decodedToken.exp && decodedToken.exp * 1000 > currentDate.getTime());
   }
 }
 
