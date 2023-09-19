5ab00be3cea4bbf222ea98dd2c55862ff1211e4d
Landon Thull <landonthull.tx@gmail.com>
2023-09-19 16:56:19 -0500

    feat: use protected route wrapper in page wrapper

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,13 +1,17 @@
 import React, {ReactElement} from "react";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx";
 
 interface IProps {
   children: ReactElement | ReactElement[];
+  requireAuth?: boolean;
 }
 
-const PageWrapper: React.FC<IProps> = ({children}) => {
+const PageWrapper: React.FC<IProps> = ({children, requireAuth = false}) => {
   return (
       <div className="bg-background text-primary max-w-screen">
+        <ProtectedRoute requireAuth={requireAuth}>
         {children}
+        </ProtectedRoute>
       </div>
   )
 }
