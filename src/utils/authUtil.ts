966096341de868e55beb688c794c44fdd6551d20
Landon Thull <landonthull.tx@gmail.com>
2023-09-21 16:52:13 -0500

    feat: add getUserEmail function
    getUserEmail extracts the current users email from the JSON web token. Returns empty string if unable to retrieve the user's email.

--- src/utils/authUtil.ts
+++ src/utils/authUtil.ts
@@ -12,6 +12,21 @@ const authUtil = {
     const currentDate = new Date();
 
     return !(decodedToken.exp && decodedToken.exp * 1000 > currentDate.getTime());
+  },
+  getUserEmail: (): string => {
+    const token = storage.getToken();
+    if (token == undefined) {
+      return "";
+    }
+
+    const decodedToken: JwtPayload = jwtDecode(token.slice(6));
+    const userEmail = decodedToken.sub;
+
+    if (userEmail) {
+      return userEmail;
+    }
+
+    return "";
   }
 }
 
