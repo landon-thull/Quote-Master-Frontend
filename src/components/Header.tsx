96e153a4bf672c0084647e663eb9c8dd269b4d1e
Landon Thull <landonthull.tx@gmail.com>
2023-09-24 15:12:46 -0500

    feat: add user profile dropdown to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import MainNav from "@/components/MainNav.tsx";
 import {Link} from "react-router-dom";
+import UserProfileDropdown from "@/features/users/components/UserProfileDropdown.tsx";
 
 const Header: React.FC = () => {
   return (
       <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <Link
-          to="/"
-          className="text-2xl font-bold tracking-wider transition-colors hover:text-muted-foreground">
-            QUOTE MASTER
-          </Link>
-          <MainNav />
+        <div className="flex h-16 items-center justify-between px-8">
+          <div className="flex">
+            <Link
+            to="/"
+            className="text-2xl font-bold tracking-wider transition-colors hover:text-muted-foreground"
+            >
+              QUOTE MASTER
+            </Link>
+            <MainNav />
+          </div>
+          <UserProfileDropdown/>
         </div>
       </div>
   );
